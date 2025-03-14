013eda3c6f6660e104c39b35cc277645e7fcd422
Artss1 <artembasev38@gmail.com>
2025-03-15 03:21:47 +0400

    Update We Are Arts.lua

--- We Are Arts.lua
+++ We Are Arts.lua
@@ -1,5 +1,5 @@
 --[[ --This script was made by Arts, enjoy!
-
+copy > > > loadstring(game:HttpGet("https://raw.githubusercontent.com/Artss1/Flades_Hub/refs/heads/main/We%20Are%20Arts.lua"))()
 ⠏⢼⡿⣰⡿⠿⠡⠿⠿⢯⣉⠿⣿⣿⣿⣿⣿⣿⣷⣶⣿⣦⣍⠻⢿⣿⣿
 ⣼⣷⢠⠀⠀⢠⣴⡖⠀⠀⠈⠻⣿⡿⣿⣿⣿⣿⣿⣛⣯⣝⣻⣿⣶⣿⣿⣿
 ⣿⡇⣿⡷⠂⠈⡉⠀⠀⠀⣠⣴⣾⣿⣿⣿⣿⣿⣍⡤⣤⣤⣤⡀⠀⠉⠛⠿
